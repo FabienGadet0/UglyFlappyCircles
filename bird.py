cdfbf8011025480e6347845226fe7b3fdf7488e0
Fabien Gadet <fabien.gadet@epitech.eu>
2019-01-13 20:00:26 +0100

    added NN

--- bird.py
+++ bird.py
@@ -2,7 +2,29 @@ from config import *
 import pygame
 from pygame.locals import *
 import random
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import losses
+from keras import optimizers
+# from keras.layers import Input
+# import keras
+import numpy as np
+
 class Bird():
+
+    def init_model(self):
+        self.model = Sequential()
+        self.model.add(Dense(4,  activation='relu', input_dim=1, kernel_initializer='RandomNormal'))
+        # self.model.add(Dense(4,  activation='relu', input_dim=1, kernel_initializer='RandomNormal'))
+        # self.model.add(Dense(4,  activation='relu', input_dim=1, kernel_initializer='RandomNormal'))
+        self.model.add(Dense(1, activation='sigmoid',kernel_initializer='RandomNormal'))
+        self.model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+        self.model.compile(loss=losses.categorical_crossentropy,
+              optimizer=optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
+
+
 
     def __init__(self, x, y):
         self.x , self.y = int(x) , int(y)
@@ -10,9 +32,18 @@ class Bird():
         self.velocity = GRAVITY
         self.jump = 0
         self.jumping = False
-        self.collid = Rect(self.x - self.radius, self.y - self.radius, self.radius / 0.5 , self.radius / 0.5)
+        self.collid = Rect(self.x - self.radius, self.y - self.radius, (self.radius / 2), (self.radius / 2))
         self.isDead = False
         self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.init_model()
+        self.fitness = 0
+
+    def brainDEAD(self, a,b,c):
+        inputs = np.array([a,b,c, self.velocity])
+        r = self.model.predict(inputs)
+        print(r.mean())
+        if r.mean() <= 0.5:
+            self.up()
 
     def pos(self):
         return (self.x, int(self.y))
@@ -24,7 +55,7 @@ class Bird():
 
     def move_y(self):
         self.y += self.velocity
-        self.collid = Rect(self.x - self.radius, self.y - self.radius, self.radius / 0.5 , self.radius / 0.5)
+        self.collid = Rect(self.x , self.y , (self.radius / 1.5), (self.radius / 1.5))
 
     def collision(self, pipe):
         if self.collid.collidelist(pipe.rect()) != -1:
@@ -34,9 +65,10 @@ class Bird():
     def update(self, delta):
         if self.y > SIZE[1] or self.y < 0:
             self.isDead = True
-        if self.jumping:
-            self.velocity -= GRAVITY  * delta
-        if self.y <= (self.jump - JUMP_HEIGHT):
-            self.velocity = GRAVITY
-            self.jumping = False
-        self.move_y()
+        if not self.isDead:
+            if self.jumping:
+                self.velocity -= GRAVITY  * delta
+            if self.y <= (self.jump - JUMP_HEIGHT):
+                self.velocity = GRAVITY
+                self.jumping = False
+            self.move_y()
--- main.py
+++ main.py
@@ -6,72 +6,117 @@ import random
 from config import *
 from pipe import *
 from bird import *
-
+import pprint
 
 def add_pipes(pipes):
     pipe_x = 150
+    p = []
     if (len(pipes) > 1):
         pipe_x = pipes[-1].x
     while pipe_x < SIZE[0] + 200:
         pipe_x += DISTANCE_BT_PIPES
         pipes.append(Pipe(pipe_x))
+  
+
+def add_birds():
+    b = []
+    for i in range(NB_BIRDS):
+        b.append(Bird(SIZE[0] / 6, SIZE[1] / random.uniform(1, 2)))
+    return b
+
+def reset(deadBirds):
+    birds = []
+    birds = add_birds()
+    deadBirds = list(filter(lambda x: x.fitness != 0, deadBirds))
+    if len(deadBirds) == 0:
+        print('all dumb')
+    if len(deadBirds) != 0:
+        deadBirds.sort(key=lambda x: x.fitness, reverse=True)
+        for idx, deadBird in enumerate(deadBirds):
+            birds[idx].model.set_weights(deadBird.model.get_weights())
+    return birds
+
+def loop(pipes, birds, screen):
+    done = False
+    every_sec = 0
+    score = 0
+    display = 0
+    clock = pygame.time.Clock()
+    deadBirds = []
+    while not done:
+        delta = clock.tick(FPS)
+        display += delta 
+        every_sec += delta
+        delta /= 50
+
+        if(len(birds) == 0):
+            score = 0
+            birds = reset(deadBirds)
+            # birds = add_birds()
+            # pipes, birds = init_all()
+   
+        for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                done = True # Flag that we are done so we exit this loop
+              
+
+
+        for idx, bird in enumerate(birds):
+            if (every_sec >= 150):
+                bird.brainDEAD(pipes[0].height, pipes[0].bot_rect.top, pipes[0].x)
+                every_sec = 0
+            bird.update(delta)
+            if bird.isDead:
+                bird.fitness = score
+                deadBirds.append(birds.pop(idx))
+
+     
+
+        for idx, pipe in enumerate(pipes):
+            pipe.update()
+            for bird in birds:
+               bird.collision(pipe)
+            if pipe.isdead:
+                del pipes[idx]
+                add_pipes(pipes)
+                score += 1
+        screen.fill(BLACK)
+
+        if display == 1000:
+            for bird in birds:
+                if not bird.isDead:
+                    pygame.draw.circle(screen, bird.color, bird.pos(), bird.radius)
+                    # pygame.draw.rect(screen, bird.color, bird.collid)
+            for pipe in pipes:
+                for single_pipe in pipe.rect():
+                    pygame.draw.rect(screen, WHITE, single_pipe)
+
+            pygame.display.flip()
+            display = 0
+
+def init_all():
+    pipes = []
+    birds = []
+    add_pipes(pipes)
+    birds = add_birds()
+    return pipes, birds
 
 def main():
 
     # Initialize the game engine
     pygame.init()
-    pause = False
-   
+
+    
     screen = pygame.display.set_mode(SIZE)
     
     pygame.display.set_caption("Flapp tamere")
-    
     #Loop until the user clicks the close button.
-    done = False
-    clock = pygame.time.Clock()
-    birds = list()
-    bird = Bird(SIZE[0] / 6, SIZE[1] / 1.5)
-    pipes = list()
-    add_pipes(pipes)
-    for i in range(NB_BIRDS):
-        birds.append(Bird(SIZE[0] / 6, SIZE[1] / random.uniform(1, 2)))
-    while not done:
-        delta = clock.tick(FPS)
-        delta /= 50
-   
-        for event in pygame.event.get(): # User did something
-            if event.type == pygame.QUIT: # If user clicked close
-                done=True # Flag that we are done so we exit this loop
-            if event.type == pygame.KEYUP:
-                for bird in birds:
-                    bird.up()
-
-
-        for idx, bird in enumerate(birds):
-            bird.update(delta)
-            if bird.isDead:
-                del birds[idx]
-     
-
-        for idx, pipe in enumerate(pipes):
-            pipe.update()
-            if pipe.isdead:
-                del pipes[idx]
-                add_pipes(pipes)
-            for bird in birds:
-               bird.collision(pipe)
-        screen.fill(BLACK)
-
- 
-        for bird in birds:
-            pygame.draw.circle(screen, bird.color, bird.pos(), bird.radius)
-            # pygame.draw.rect(screen, bird.color, bird.collid)
-        for pipe in pipes:
-            for single_pipe in pipe.rect():
-                pygame.draw.rect(screen, WHITE, single_pipe)
-        # Go ahead and update the screen with what we've drawn.
-        # This MUST happen after all the other drawing commands.
-        pygame.display.flip()
+    
+    
+    pipes, birds = init_all()
+    
+    loop(pipes, birds, screen)
+    
 
     pygame.quit()
 
